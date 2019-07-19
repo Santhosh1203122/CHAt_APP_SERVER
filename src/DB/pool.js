09a99ed99d9854518e7b32eff12a0b6307f5ba00
Santhosh1203122 <rm.santhosh1234@gmail.com>
2019-07-20 02:51:17 +0530

    updating database

--- src/DB/pool.js
+++ src/DB/pool.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql2/promise');
 // mysql://b46ad1b134067f:2f11892a@us-cdbr-iron-east-02.cleardb.net/heroku_b55b0905485ee8f?reconnect=true
 const mysqlConfig = {
-    host: 'us-cdbr-iron-east-02.cleardb.net',
-    user: 'b46ad1b134067f', // point out to the DB which you are using
-    password: '2f11892a', // provide the password for the same
-    database: 'heroku_b55b0905485ee8f'
+    host: 'otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'gcsgt9ttu4riqvpt', // point out to the DB which you are using
+    password: 'zbs4ivpah09o727u', // provide the password for the same
+    database: 'izifjivadvjuhvfh'
 }
 
 module.exports = {
